0c80290baa10d8675f3f87a7e718f0008a39fff5
Amine-bc <lm_bouchoucha@esi.dz>
2023-05-02 10:31:03 +0100

    Deploying push_

--- src/assembler/SyntaxicAnalysis.js
+++ src/assembler/SyntaxicAnalysis.js
@@ -87,7 +87,7 @@ export class SyntaxicAnalysis {
                         // ONE params instructions: INST1 ::=  NEG, NOT, SHL, SHR, READ, WRITE, PUSH, POP, ROR, ROL, CALL, BE, BNE, BS, BI, BIE, BSE, BR
                         //|                                                                                         |
                         //|        Must have only one other param: it must be valid                                 |
-                        //|        or one param and other special chars: they must be valid  also                   |
+                        //|        or one param and other special chars: they must be valid  also  .                 |
                         //|        That other special char is used for addressing modes mainly    .                  |
                         //|------------------------------------------------------------------------------------------
                         const functINST1 = ()=> {
