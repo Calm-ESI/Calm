8158d2c8bac741edccc35626cf15b6f42c02e7c7
Ayyoub Kasmi <ayyoubkasmidz@gmail.com>
2023-05-17 22:34:29 +0200

    Fixed a bug in the exercices page

--- src/pages/ExercicesPage/index.jsx
+++ src/pages/ExercicesPage/index.jsx
@@ -25,16 +25,16 @@ function ExercicesPage(props) {
                     This exercises are ranked by level so you can test yourself according to what you have learned and to the
                     difficulty of the exercises.
                 </p>
-                <Learn title={"Level 1"} link={"/learn/Exercices/level1"} text={"Embark on a learning journey with our beginner-friendly " +
+                <Learn buttonText="Practice" title={"Level 1"} but link={"/learn/Exercices/level1"} text={"Embark on a learning journey with our beginner-friendly " +
                     "course and uncover the inner workings of the calM machines."} img={starImage}/>
 
-                <Learn title={"Level 2"} link={"/learn/Exercices/level2"} img={star2Image} text={"Discover how data is" +
+                <Learn buttonText="Practice" title={"Level 2"} link={"/learn/Exercices/level2"} img={star2Image} text={"Discover how data is" +
                     " stored and retrieved from the memory and the different modes used to access information."}/>
 
-                <Learn title={"Level 3"} link={"/learn/Exercices/level3"} img={star3Image} text={"Get to know the different " +
+                <Learn buttonText="Practice" title={"Level 3"} link={"/learn/Exercices/level3"} img={star3Image} text={"Get to know the different " +
                     "parts that makes the calM machine and how they interact with each other."}/>
 
-                <Learn title={"Level 4"} img={star4Image} text={"A collection of code examples we designed carefully to show you how the calM machine" +
+                <Learn buttonText="Practice" title={"Level 4"} img={star4Image} text={"A collection of code examples we designed carefully to show you how the calM machine" +
                     "works in action, you can execute them !"} link={"/learn/Exercices/level4"}/>
             </div>
             <Bot/>
